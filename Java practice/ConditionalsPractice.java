4a947525c533668faccb4af4f207fe983d1fc716
Faizan Parabtani <faizan11911@gmail.com>
2025-02-26 11:42:43 -0500

    Moved folders datastructures, algos and datastructres in java out of leetcode folder

---
+++ Java practice/ConditionalsPractice.java
@@ -0,0 +1,9 @@
+// default class - only accessible to classes in the same package
+public class ConditionalsPractice {
+	
+	public static void main(String args[]){
+		int a = 3;
+
+		System.out.println(a);
+	}
+}
---
+++ Java practice/Geeks.java
@@ -0,0 +1,17 @@
+// == operator for compatible data types
+class Geeks {
+    public static void main(String[] args) {
+        
+        // integer-type
+        System.out.println(10 == 10);
+
+        // char-type
+        System.out.println('a' == 'b');
+
+        // char and double type
+        System.out.println('a' == 97.0);
+
+        // boolean type
+        System.out.println(true == false);
+    }
+}
